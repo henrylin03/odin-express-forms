ab468936f1ea1317db792d3f9b7af4f30a1eb897
Henry <83106787+henrylin03@users.noreply.github.com>
2025-07-01 20:51:22 +1000

    chore: setup initial files. setup packages.

---
+++ README.md
@@ -0,0 +1,3 @@
+# odin-express-forms
+
+Project to practice implementing forms and data handling with Express.js in Node.js
---
+++ storages/usersStorage.js
@@ -0,0 +1,34 @@
+// this class simulates interacting with db
+
+class UsersStorage {
+  constructor() {
+    this.storage = {};
+    this.id = 0;
+  }
+
+  addUser({ firstName, lastName }) {
+    const id = this.id;
+    this.storage[id] = { id, firstName, lastName };
+    this.id++;
+  }
+
+  getUsers() {
+    return Object.values(this.storage);
+  }
+
+  getUser(id) {
+    return this.storage[id];
+  }
+
+  updateUser(id, { firstName, lastName }) {
+    this.storage[id] = { id, firstName, lastName };
+  }
+
+  deleteUser(id) {
+    delete this.storage[id];
+  }
+}
+
+// export _instance_ of class rather than exporting the class
+// ensures only _one instance_ of class can exist ("singleton pattern")
+module.exports = new UsersStorage();
