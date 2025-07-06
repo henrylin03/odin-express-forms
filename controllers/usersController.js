fd94270a5a1ba52588ffa0f212df158bfc9fb87d
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-07-07 08:53:05 +1000

    feat: add validation to form fields. add sanitisation to form values. display errors in /create page if there are errors on submit

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,20 +1,50 @@
+const { body, validationResult } = require("express-validator");
 const usersStorage = require("../storages/usersStorage");
 
-exports.usersListGet = (req, res) => {
+const alphaErr = "must only contain letters.";
+const lengthErr = "must be between 1 and 10 characters";
+
+const validateUser = [
+  body("firstName")
+    .trim()
+    .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`First name ${lengthErr}`),
+
+  body("lastName")
+    .trim()
+    .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
+    .isLength({ min: 1, max: 10 })
+    .withMessage(`Last name ${lengthErr}`),
+];
+
+exports.usersListGet = (_req, res) => {
   res.render("index", {
     title: "User list",
     users: usersStorage.getUsers(),
   });
 };
 
-exports.usersCreateGet = (req, res) => {
+exports.usersCreateGet = (_req, res) => {
   res.render("createUser", {
     title: "Create users",
   });
 };
 
-exports.usersCreatePost = (req, res) => {
-  const { firstName, lastName } = req.body;
-  usersStorage.addUser({ firstName, lastName });
-  res.redirect("/");
-};
+exports.usersCreatePost = [
+  validateUser,
+  (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty())
+      return res
+        .status(400)
+        .render("createUser", { title: "Create user", errors: errors.array() });
+
+    const { firstName, lastName } = req.body;
+    usersStorage.addUser({ firstName, lastName });
+    res.redirect("/");
+  },
+];
